756038acb84910527b8fcae7089fd898043f7d3a
Barry Cann <barrycann@gmail.com>
2017-01-16 14:52:36 -0700

    Get friends app complete with add-friend functionality

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,4 +1,31 @@
 angular.module('friendsList')
 .controller('mainCtrl', function($scope){
+   $scope.friends = [
+      {
+         name: 'Mike',
+         birthday: 'Apr 10'
+      },
+      {
+         name: 'Scott',
+         birthday: 'Mar 16'
+      },
+      {
+         name: 'David',
+         birthday: 'Jan 6'
+      },
+      {
+         name: 'Levi',
+         birthday: 'Aug 29'
+      }
+   ];
 
+   $scope.addFriend = function(name, birthday){
+      var newFriend = {  
+         name: name,
+         birthday: birthday
+      }
+      $scope.friends.push(newFriend);
+      $scope.newFriendName = '';
+      $scope.newFriendBirthday = '';
+   }
 });
